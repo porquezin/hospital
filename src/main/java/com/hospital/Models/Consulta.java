eceac1041fef9dc4c0ce9f951fcdcd545bbaf991
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2022-09-28 22:30:25 -0300

    Criando tabela consulta

--- src/main/java/com/hospital/Models/Consulta.java
+++ src/main/java/com/hospital/Models/Consulta.java
@@ -1,5 +1,38 @@
 package com.hospital.Models;
 
 public class Consulta {
-    
+    private int IdConsulta;
+    private int IdMedico;
+    private int IdPaciente;
+    private String descricao;
+    public Consulta(int idConsulta, int idMedico, int idPaciente, String descricao) {
+        IdConsulta = idConsulta;
+        IdMedico = idMedico;
+        IdPaciente = idPaciente;
+        this.descricao = descricao;
+    }
+    public int getIdConsulta() {
+        return IdConsulta;
+    }
+    public void setIdConsulta(int idConsulta) {
+        IdConsulta = idConsulta;
+    }
+    public int getIdMedico() {
+        return IdMedico;
+    }
+    public void setIdMedico(int idMedico) {
+        IdMedico = idMedico;
+    }
+    public int getIdPaciente() {
+        return IdPaciente;
+    }
+    public void setIdPaciente(int idPaciente) {
+        IdPaciente = idPaciente;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
